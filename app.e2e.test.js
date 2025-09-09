b7705a79e42edfbd49563fb547041da9eaef1c31
Berry Langerak <blangerak@tilaa.com>
2025-09-09 16:31:56 +0200

    Capitals are important.

--- app.e2e.test.js
+++ app.e2e.test.js
@@ -7,7 +7,7 @@ describe('API Tests', () => {
   it('should return Hello World from GET /', async () => {
     const res = await request(baseURL).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello world!');
+    expect(res.text).toBe('Hello World!');
   });
 
   it('should respond within reasonable time', async () => {
